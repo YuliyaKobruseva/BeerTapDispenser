7f42afb139247a9a3a8926adf2c7254a53ad7ce3
Yuliya Kobruseva <juliakobruseva@gmail.com>
2024-03-14 12:16:26 +0100

    Resolve error in handlerException

--- src/main/java/com/rviewer/skeletons/domain/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/rviewer/skeletons/domain/exceptions/GlobalExceptionHandler.java
@@ -43,7 +43,7 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
-    @ExceptionHandler(InvalidStatusException.class)
+    @ExceptionHandler(InvalidRequestException.class)
     public ResponseEntity<String> handleInvalidRequestException(InvalidRequestException ex) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
